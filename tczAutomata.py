999f0d48c2711dd54549b7f3267cf2bb40265ee8
Marco Zunino <eng.marco.zunino@gmail.com>
2015-08-12 15:39:52 +0200

    Explored the problem with Automata a little more, added some comment on this topic but not solved yet, committing just to be able to work on this at home

--- tczAutomata.py
+++ tczAutomata.py
@@ -32,18 +32,50 @@ class TCPConnection(object):
 		self.currSeq = 111250
 
 		# TODO what happen with self.srcPort???
-		self.tcpAuto = TCP_client.tcplink(Raw, self.srcIp, self.dstPort)
+		print "\n\t[DEBUG]  creating an instance of TCP_client."
+		self.tcpAuto = TCP_client.tcplink(Raw, self.dstIp, self.dstPort)
+		print "\n\t[DEBUG]  after instance creation"
 
 
-	def send(self, payload):
+		print "\n\t[DEBUG]  Trying to send some data directly after creation, boia faust!!"
+		self.tcpAuto.send("Ginger11111")
+
+	def sendt(self, payload):
+		print "\n\t[DEBUG]  about to send the payload with the wrapper"
 		self.tcpAuto.send(payload) 
 
 
+conf.L3socket=L3RawSocket
+
 ginger = TCPConnection("127.0.0.1", 6666)
-ginger.tcpAuto.send("Piciu")
+print "\t\n[DEBUG]  After completing call to create instance"
+
+ginger.sendt("Piciu")
+print "\t\n[DEBUG]  Dopo aver inviato i dati con il wrapper"
+
+# Trying here to send directly without using the TCPConnection obkect
+t = TCP_client.tcplink(Raw, "127.0.0.1", 6665)
+t.send("Puttana di Eva!!!")
+
 
 # [22.07.2015 M. Zunino] Interrupted here, this is not working, even if conf is correct and the RST packets are
 # correctly blocked. In some cases nothing is sent at all, in other cases only the SYN is sent but no response
 # from the listening instance of netcat.
 # Itis a little strange because the exact same code run from the scapy interactive console works (but also this not
 # always, so I believe it is some kind of general problem of the system, need to check (all was run as root)
+
+# [12.08.2015] I think the problem can be related with the `conf` object, if I use directly the TCP_client object
+# and try to send shit to "www.google.com", I see at least the DNS request and maybe also SYN, nothing happen if
+# I try to send in any way on the local interface, regardless of the conf.L3socket settings.
+
+
+
+# I insert the code to close the stdout and stderr to avoid the "sys.excepthook is missing" error with Scapy 2.2.0
+try:
+    sys.stdout.close()
+except:
+    pass
+try:
+    sys.stderr.close()
+except:
+    pass
