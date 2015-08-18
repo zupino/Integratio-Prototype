c7d912e588523d79f063fbad7619dc0583958c3e
Marco Zunino <eng.marco.zunino@gmail.com>
2015-08-18 16:47:10 +0200

    It seems like the delay solve the send() problem because it give the 3-way handshake the time to complete before actually sending data. I added here a draft function to wait on the Automaton state transiction before sending data, there should be such a control in the TCP_client itself but I could not find it at the moment. I might incorporate this check in the TCPConnection class itself (I do not want to modify the source code of Scapy, if I can confirm my understanding I might prepare a push request.

--- ginger.py
+++ ginger.py
@@ -2,6 +2,7 @@
 
 from scapy.all import *
 import time
+import sys
 
 print conf.iface
 
@@ -12,18 +13,39 @@ class TCPConnection(object):
 		self.add = ad
 		self.port = po
 		self.t = TCP_client.tcplink(Raw, self.add, self.port)
+		time.sleep(2)	
 	def sendInt(self, payload):
 		self.t.send(payload)
 	
-print "\n\t[DEBUG] Creation of TCPConnection object"
-conn = TCPConnection("127.0.0.1", 8090)
-time.sleep(2)
-print "\n\t[DEBUG] About to send the message to port 8090 on localhost"
+	def waitForConnection(self, timeout, period):
+		mustend = time.time() + timeout
+		while time.time() < mustend:
+			if (self.t) and (self.t.atmt.state.state == 'ESTABLISHED'): return True
+			time.sleep(period)
+		return False
+
+
+conn = TCPConnection("127.0.0.1", 8091)
+
+# Just a try for a waiting function
+print "\n\t[DEBUG] Entering the check status function"
+mustend = time.time() + 5
+while time.time() < mustend:
+	if conn.t.atmt.state.state == 'ESTABLISHED': 
+		print "\t[DEBUG] Now the connection is established, leaving the waiting loop"
+		break	
+	else:
+		print "\t[DEBUG] not established yet. Status is: " . conn.t.atmt.state.state
+		time.sleep(0.001)
+
+
+
 conn.sendInt("GET / HTTP/1.0\r\n\r\n")
 time.sleep(2)
 conn.t.close()
-
+time.sleep(1)
 #tcp = TCP_client.tcplink(Raw, "www.google.com", 11111)
 #tcp.send("ginger")
 
 # I insert the code to close the stdout and stderr to avoid the "sys.excepthook is missing" error with Scapy 2.2.0
+
