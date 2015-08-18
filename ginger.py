b967127cf69455524c76c67ce14f722a40a9f12e
Marco Zunino <eng.marco.zunino@gmail.com>
2015-08-18 13:52:56 +0200

    Managed to solve the issue with the Automata not working in the script (but working from interactive shell): if delay are introduced between the creation of the object and the send of data, eveything works fine. I will keep investigate this issue but for the moment I will use this workaround not to be blocked.

--- ginger.py
+++ ginger.py
@@ -1,4 +1,9 @@
+#!/usr/bin/python
+
 from scapy.all import *
+import time
+
+print conf.iface
 
 conf.L3socket = L3RawSocket
 
@@ -9,20 +14,16 @@ class TCPConnection(object):
 		self.t = TCP_client.tcplink(Raw, self.add, self.port)
 	def sendInt(self, payload):
 		self.t.send(payload)
-
-conn = TCPConnection("www.google.com", 80)
+	
+print "\n\t[DEBUG] Creation of TCPConnection object"
+conn = TCPConnection("127.0.0.1", 8090)
+time.sleep(2)
+print "\n\t[DEBUG] About to send the message to port 8090 on localhost"
 conn.sendInt("GET / HTTP/1.0\r\n\r\n")
+time.sleep(2)
+conn.t.close()
 
 #tcp = TCP_client.tcplink(Raw, "www.google.com", 11111)
 #tcp.send("ginger")
 
 # I insert the code to close the stdout and stderr to avoid the "sys.excepthook is missing" error with Scapy 2.2.0
-try:
-    sys.stdout.close()
-except:
-    pass
-try:
-    sys.stderr.close()
-except:
-    pass
-
