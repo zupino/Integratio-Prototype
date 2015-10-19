c1e2e357bb358944a456e6aaa335771efcb5c5da
Marco Zunino <eng.marco.zunino@gmail.com>
2015-10-18 23:23:22 -0700

    Some additional work on what it is still called prove.py, TCZee based on Automata. Now the state machine loops so that after transmitting it goes to LISTEN state again. Also HTTP response was correctly received from client side, but still need to check the case of HTTP request splitted on multiple segments.

--- prove.py
+++ prove.py
@@ -154,7 +154,18 @@ class TCZee(Automaton):
 			# DEBUG
 			print "\t\t[DEBUG] in the condition, pkt is a FIN so I raise CLOSING state"
 			print "\t\t[DEBUG] Content of pkt: " + pkt.summary()
-			raise self.CLOSING()
+			# Adjusting the seq and ack, in case of handshake and closing, there is an
+			# 
+			self.l3[TCP].seq = pkt[TCP].ack
+			self.l3[TCP].ack = pkt[TCP].seq + 1
+	
+			self.l3[TCP].flags = 'FA'
+			self.last_packet = self.l3
+			self.send(self.last_packet)
+			# TODO 	Trying here to just go again in ESTABLISHED state, as there is an handler 
+			# 	for the FIN packets there. Should maybe go to some CLOSING state, for the 
+			# 	moment will go like this.
+			raise self.LISTEN()
 
 		# check if the received packet is a PSH/ACK 0x18
 		# TODO 	EPIC this will need to consider also the case of HTTP requests splitted over
@@ -179,8 +190,10 @@ class TCZee(Automaton):
 				if "GET /" in pkt[TCP].load:
 					# DEBUG
 					print "\t\t\t[DEBUG] Got a / request, preparing response"
-					
-					httpResponse = "HTTP/1.1 200 OK\x0d\x0aServer: Integratio Test Server\x0d\x0aConnection: Keep-Alive\x0d\x0aContent-Type: text/html; charset=UTF-8\x0d\x0aContent-Length: 849\x0d\x0a\x0d\x0a<html><head><title>Integratio - M. Zunino 2014</title></head><body><h2><span style='font-family:georgia,serif;'>Integratio - HTTP Test Server</span></h2><p><p><span style='color:#800080;'><span style='font-size:14px;'><em><span style='font-family:georgia,serif;'>The Integration project is an Open Source project started in 2014 by Marco Zunino.&nbsp;<a href='https://github.com/zupino/tcz'>https://github.com/zupino/tcz</a></span></em></span></span></p><span style='font-family:georgia,serif;'>This is a&nbsp;<em>really simple, </em><a href='https://bitbucket.org/secdev/scapy/src'>Scapy</a>-based implementation of a HTTP Web Server. The scope of this tool is to provide a framework to test the client robustness to <strong>network delay</strong>, <strong>malformed response </strong>and <strong>error condition</strong>.</span></p></body></html>"
+					httpBody = "<html><head><title>Integratio - M. Zunino 2014</title></head><body><h2><span style='font-family:georgia,serif;'>Integratio - new HTTP Test Server</span></h2><p><p><span style='color:#800080;'><span style='font-size:14px;'><em><span style='font-family:georgia,serif;'>The Integration project is an Open Source project started in 2014 by Marco Zunino.&nbsp;<a href='https://github.com/zupino/tcz'>https://github.com/zupino/tcz</a></span></em></span></span></p><span style='font-family:georgia,serif;'>This is a&nbsp;<em>really simple, </em><a href='https://bitbucket.org/secdev/scapy/src'>Scapy</a>-based implementation of a HTTP Web Server. The scope of this tool is to provide a framework to test the client robustness to <strong>network delay</strong>, <strong>malformed response </strong>and <strong>error condition</strong>.</span></p></body></html>"
+					httpHeader = "HTTP/1.1 200 OK\x0d\x0aServer: Integratio Test Server\x0d\x0aConnection: Keep-Alive\x0d\x0aContent-Type: text/html; charset=UTF-8\x0d\x0aContent-Length: " + str(len(httpBody)) + "\x0d\x0a\x0d\x0a"
+					httpResponse = httpHeader + httpBody 
+
 				elif "GET /somethingElse.html" in pkt[TCP].load:
 					# DEBUG
 					print "\t\t\t[DEBUG] Got a /somethingElse.html request, preparing response"
@@ -198,7 +211,7 @@ class TCZee(Automaton):
 			# automatically with Scapy magic
 			# TODO seems like sending the ACK for the request and the response in the same TCP segment
 			# is not ok, trying to send them separatelly
-			# self.l3[TCP].load = httpResponse
+			# self.l3[TCP] = httpResponse
 			
 			# TODO 	it is still not clear what is better in such a situation: put here the send() call
 			# 	or put it in the action related to this state? Question arises because based on
@@ -208,16 +221,18 @@ class TCZee(Automaton):
 			#	where also the state change.
 			# DEBUG
                 	print "\t\t[DEBUG] [ESTABLISHED] Sending the ACK"
-                	self.last_packet = self.l3
+                	self.last_packet = self.l3/httpResponse
                	 	self.send(self.last_packet)
-        	        
-			self.l3[TCP].load = httpResponse
-			self.last_packet = self.l3
-			self.send(self.last_packet)
+        	       	
+ 			# sending the actual HTTP response
+
+			#self.l3[TCP].load = httpResponse
+			#self.last_packet = self.l3
+			#self.send(self.last_packet)
 			
 			
 			# DEBUG
-	                print "\t\t\t[DEBUG] content of response sent: " + self.last_packet[TCP].load
+	                #print "\t\t\t[DEBUG] content of response sent: " + self.last_packet[TCP].load
 			print "\t\t[DEBUG] [ESTABLISHED]  data ACK sent, back to ESTABLISHED now: " + self.last_packet.summary()	
 			raise self.ESTABLISHED()
 		else:
@@ -231,6 +246,8 @@ class TCZee(Automaton):
 		#DEBUG 
 		print "[DEBUG] entering [CLOSING]"
 		
+
+	
 
 		
 
