c69a7889a788c401b47144e3cbdf8cf974a0cbae
Marco Zunino on Raspberry <info@marcozunino.it>
2015-11-23 01:07:36 +0000

    Just one test to clarify a git behavior checking out directly from RPi

--- tcz.py
+++ tcz.py
@@ -31,6 +31,7 @@
 # providing answer to the requests of the tested IoT product in such a way to
 # stress the desired error condition.
 
+# Last change right now 17:06 of 22 November 2015
 
 from scapy.all import *
 # from tcz import TCPConnection
