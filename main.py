334264387e74bf6714366766e96a6f3bca4c5be9
Bharadwaj Desikan <bharadwajdes3@gmail.com>
2016-02-20 22:51:04 +0100

    updated basic structure to comply with solution architecture Wiki.

--- main.py
+++ main.py
@@ -6,27 +6,6 @@ from tester import *
 # To be used when working on local interface l0
 conf.L3socket = L3RawSocket
 
-class TestComponent(object):
-	
-	def __init__(self):
-		self.testServers=[]
-
-	def jsonParse(self, file='test.json'):
-		''' Function to be completed by raja for Issue #5 to parse a specific 
-		json file and return as python dictionary.'''
-		pass
-
-	def createTestServer(self, jsonDict=None):
-		''' Function calls the Tester Component defined in Tester.py'''
-		tester = Tester(jsonDict)
-	
-	def run(self):
-		''' Function which search for the list of json files in the specified 
-		configuration folder and calls the jsonParse function in loops. This 
-		is also part of Issue #5. '''
-		pass
-
-
 if __name__=='__main__':
-	componentCreate = TestComponent()
+	componentCreate = Tester()
 	componentCreate.run()
--- tester.py
+++ tester.py
@@ -3,7 +3,29 @@ from _pyio import __metaclass__
 
 '''Approach for Test Server using the Meta classes.'''
 
-class TesterMeta(type):
+
+class Tester(object):
+    
+    def __init__(self):
+        self.testServers=[]
+
+    def jsonParse(self, file='test.json'):
+        ''' Function to be completed by raja for Issue #5 to parse a specific 
+        json file and return as python dictionary.'''
+        pass
+
+    def createTestServer(self, jsonDict=None):
+        ''' Function calls the Tester Component defined in Tester.py'''
+        tester = TestServer(jsonDict)
+    
+    def run(self):
+        ''' Function which search for the list of json files in the specified 
+        configuration folder and calls the jsonParse function in loops. This 
+        is also part of Issue #5. '''
+        pass
+
+
+class TesterServerMeta(type):
     def __init__(cls, name, bases, clsdict):
         jsonDict=clsdict[args]
         if jsonDict['category'] == 'time':
@@ -11,5 +33,5 @@ class TesterMeta(type):
                 clsdict[jsonDict['testType']](self)
             setattr(cls, jsonDict['testType'], new_method)
 
-class Tester(TCZee):
-    __metaclass__ = TesterMeta
+class TestServer(TCZee):
+    __metaclass__ = TesterServerMeta
