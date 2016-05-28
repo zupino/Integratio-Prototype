7cf63158474750188664af6a3d7f8cbfef9d25a7
Marco Zunino <eng.marco.zunino@gmail.com>
2016-05-28 19:33:15 +0200

    more folder cleaning

--- tcz/__init__.py
+++
@@ -1,8 +0,0 @@
-from scapy.all import *
-from tczDebug import TCZee
-import time
-
-# Just a small utility to get from system the ip of a specific network interface
-import socket
-import fcntl
-import struct
