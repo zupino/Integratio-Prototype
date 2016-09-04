2f435a79abb5249b0faa8972f4fa77cb95d39a12
zupino <eng.marco.zunino@gmail.com>
2016-09-04 13:43:30 +0000

    main.py edited online with Bitbucket

--- main.py
+++ main.py
@@ -11,15 +11,6 @@ import sys
 #conf.L3socket = L3RawSocket
 sys.path.append('.')
 
-#log_scapy = logging.getLogger("scapy")
-#console_handler = logging.StreamHandler()
-#console_handler.setFormatter(logging.Formatter("%(levelname)s: %(message)s"))
-#log_scapy.addHandler(console_handler)
-#log_runtime = logging.getLogger("scapy.runtime")          # logs at runtime
-#log_runtime.addFilter(ScapyFreqFilter())
-#log_interactive = logging.getLogger("scapy.interactive")  # logs in interactive functions
-#log_loading = logging.getLogger("scapy.loading")          # logs when loading scapy
-
 if __name__=='__main__':
     
     log_scapy.setLevel(1)
