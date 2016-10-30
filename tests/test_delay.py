8f8efdab4f92241462a81589d2c733c14f44e4fe
Marco Zunino UI Studio <eng.marco.zunino@gmail.com>
2016-10-30 21:10:03 +0000

    Added on epositive testing for simple connection

--- tests/test_delay.py
+++ tests/test_delay.py
@@ -8,6 +8,23 @@ import pytest
 import io
 import json
 import time
+import socket
+
+
+
+def connect( time = 3, server_address = ('192.168.13.1', 80) ):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(time)
+    sock.connect(server_address)
+
+# Same as connect(), just return an error code instead of 
+# raising an exception if the connection fails
+def connect_ex( time = 3, server_address = ('192.168.13.1', 80) ):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(time)
+    return sock.connect_ex(server_address)
+
+
 
 @pytest.fixture
 def runConnector():
@@ -18,14 +35,18 @@ def runConnector():
     con.runbg()
     return con
 
-#    def setup_method(self, test_content_500):
-#        fileConfig = io.open('../configs/content.json')
-#        config = json.load(fileConfig)
-#        con = tcz.Connector(config, debug=3)
-#        con.run()
+# TCZ will wait 3 seconds before reply to SYN.
+# connect has a timeout of 1 in this test case.
+# TEST PASS if the connect(1) raise the Timeout
+# exception 
 
-def test_delay(runConnector):
-    with pytest.raises(requests.exceptions.Timeout):
-        r = requests.get('http://192.168.13.1', timeout=2)
-#        assert 0
-        
+def test_delay1(runConnector):
+    with pytest.raises(socket.timeout):
+        connect(1)
+
+
+def test_delay2(runConnector):
+    assert connect_ex(5) == 0
+    
+
+
